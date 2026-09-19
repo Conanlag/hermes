7e469fd21fa5fc9e3827d39c99d4a0a7120dc5a9
Conanlag <alberto.mejia4395@alumnos.udg.mx>
2026-09-19 17:41:21 -0600

    fix: IDs guardados, leeidos para colocar el siguiente ID

--- src/hermes/src/job/job.cpp
+++ src/hermes/src/job/job.cpp
@@ -1,14 +1,44 @@
 #include "job.h"
+#include <fstream>
+
+using std::ifstream;
+using std::ofstream;
+
+
 
 Job crearJob(
     const string& programa,
     const vector<string>& argumentos
 ){
-    static unsigned int siguienteId = 1;
+    // Generar un ID por cada job creado
+    const string archivoId = "data/job_id";
+    
+    unsigned int siguienteId = 1;
+
+    // Leer el anterior Id Desde el archivo
+        ifstream archivoLectura(archivoId);
+
+    if (archivoLectura.is_open()) {
+        archivoLectura >> siguienteId;
+        archivoLectura.close();
+    }
+
+ 
+
+    // Crear el job 
     Job job;
-    job.id = siguienteId++;
+    job.id = siguienteId;
     job.programa = programa;
     job.argumentos = argumentos;
+
+
+    // Guardar el siguiente ID
+    ofstream archivoEscritura(archivoId);
+
+    if (archivoEscritura.is_open()) {
+        archivoEscritura << siguienteId + 1;
+        archivoEscritura.close();
+    }
 
     return job;
 }
